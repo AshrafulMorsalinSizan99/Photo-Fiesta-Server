375ad683787e35a11539bf883b169b0bbf99c1a1
AshrafulMorsalinSizan99 <jucse28.377@gmail.com>
2022-11-08 18:05:35 +0600

    delete done

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('photoFiesta').collection('services');
+        const reviewCollection = client.db('photoFiesta').collection('reviews');
 
         app.get('/services', async (req, res) => {
             const query = {}
@@ -41,6 +42,30 @@ async function run() {
             const query = { _id: ObjectID(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
+        });
+
+        app.get('/reviews', async (req, res) => {
+            // console.log(req.query.email);
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        });
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectID(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
         })
     }
     finally {
