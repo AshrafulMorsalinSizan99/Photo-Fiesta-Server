0ec41d9fed691a2f754c692014cb85bf201997f5
AshrafulMorsalinSizan99 <jucse28.377@gmail.com>
2022-11-08 11:15:45 +0600

    cached node_modules

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const { ObjectID } = require('bson');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.u6euqsi.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('photoFiesta').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        })
+
+        app.get('/allServices', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectID(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
+
+
+app.get('/', (req, res) => {
+    res.send('server is running');
+})
+
+app.listen(port, () => {
+    console.log(`photo fiesta running on port ${port}`);
+})
